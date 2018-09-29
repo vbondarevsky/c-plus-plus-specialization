cebe0d15bf46fc4c591335ecc1ea93e42a30fd95
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2018-09-30 00:45:56 +0300

    Задание "Средняя температура-2"

---
+++ yellow/average-temperature-2/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+int main() {
+    int N;
+    std::cin >> N;
+    std::vector<int> T(N);
+    for (auto &t : T) {
+        std::cin >> t;
+    }
+
+    int m = std::accumulate(T.begin(), T.end(), 0ll) / N;
+
+    std::vector<int> filteredT;
+    for (auto i = 0; i < T.size(); ++i) {
+        if (T[i] > m) {
+            filteredT.push_back(i);
+        }
+    }
+    std::cout << filteredT.size() << std::endl;
+    for (const auto &t : filteredT) {
+        std::cout << t << ' ';
+    }
+
+    return 0;
+}
