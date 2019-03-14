7f4ee5098429cd5bc09a5235be767f137bb12f97
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2019-03-15 01:42:28 +0300

    Задание "Тесты для функции IsPalindrom"

---
+++ yellow/test_palindrome/main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& x : s) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << x;
+    }
+    return os << "}";
+}
+
+template<class T>
+ostream& operator<<(ostream& os, const set<T>& s) {
+    os << "{";
+    bool first = true;
+    for (const auto& x : s) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << x;
+    }
+    return os << "}";
+}
+
+template<class K, class V>
+ostream& operator<<(ostream& os, const map<K, V>& m) {
+    os << "{";
+    bool first = true;
+    for (const auto& kv : m) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << kv.first << ": " << kv.second;
+    }
+    return os << "}";
+}
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+    if (t != u) {
+        ostringstream os;
+        os << "Assertion failed: " << t << " != " << u;
+        if (!hint.empty()) {
+            os << " hint: " << hint;
+        }
+        throw runtime_error(os.str());
+    }
+}
+
+void Assert(bool b, const string& hint) {
+    AssertEqual(b, true, hint);
+}
+
+class TestRunner {
+public:
+    template<class TestFunc>
+    void RunTest(TestFunc func, const string& test_name) {
+        try {
+            func();
+            cerr << test_name << " OK" << endl;
+        } catch (exception& e) {
+            ++fail_count;
+            cerr << test_name << " fail: " << e.what() << endl;
+        } catch (...) {
+            ++fail_count;
+            cerr << "Unknown exception caught" << endl;
+        }
+    }
+
+    ~TestRunner() {
+        if (fail_count > 0) {
+            cerr << fail_count << " unit tests failed. Terminate" << endl;
+            exit(1);
+        }
+    }
+
+private:
+    int fail_count = 0;
+};
+
+bool IsPalindrom(const string& str) {
+    // Вы можете вставлять сюда различные реализации функции,
+    // чтобы проверить, что ваши тесты пропускают корректный код
+    // и ловят некорректный
+}
+
+void TestAll() {
+    Assert(IsPalindrom(""), "Empty string is palindrome");
+    Assert(IsPalindrom(" "), "Space is palindrome");
+    Assert(IsPalindrom("  "), "Space is palindrome");
+    Assert(IsPalindrom("a"), "One symbol is palindrome");
+    Assert(IsPalindrom("aa"), "String aa is palindrome");
+    Assert(IsPalindrom("ada"), "String is palindrome");
+    Assert(IsPalindrom("daad"), "String is palindrome");
+    Assert(IsPalindrom(" da ad "), "String is palindrome");
+    Assert(!IsPalindrom("Daad"), "String Daad is not palindrome");
+    Assert(!IsPalindrom("ada "), "Correct comparison");
+    Assert(!IsPalindrom(" ada"), "Correct comparison");
+    Assert(!IsPalindrom("ada aada"), "String is not palindrome");
+}
+
+int main() {
+    TestRunner runner;
+    runner.RunTest(TestAll, "TestAll");
+
+    return 0;
+}
