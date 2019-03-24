3eb1bac89559b80b2ad6c0a9f926bee11c646c46
Vladimir Bondarevskiy <vbondarevsky@gmail.com>
2019-03-24 03:10:39 +0300

    Задание "Плохой макрос"

---
+++ red/week1/bad_macro/print_values.cpp
@@ -0,0 +1,24 @@
+#include "test_runner.h"
+
+#include <ostream>
+
+using namespace std;
+
+#define PRINT_VALUES(out, x, y) out << (x) << endl << (y) << endl
+
+int main() {
+    TestRunner tr;
+    tr.RunTest([] {
+        ostringstream output;
+        PRINT_VALUES(output, 5, "red belt");
+        ASSERT_EQUAL(output.str(), "5\nred belt\n");
+    }, "PRINT_VALUES usage example");
+
+    ostringstream output;
+    if (false)
+        PRINT_VALUES(output, 1, 0);
+
+    tr.RunTest([&output] {
+        ASSERT_EQUAL(output.str(), "");
+    }, "PRINT_VALUES if");
+}
